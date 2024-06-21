eb2f6b7fb8fb2d5b4e629d6632da1f779407f145
VIOLET-EVERGARDE <kotakandis185@gmail.com>
2024-06-22 02:12:14 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -9,7 +9,7 @@ export BRANCH="android-9.0" # the branch of manifest
 export DEVICE="CPH2083" # codename used in device tree
 export DT_LINK="https://github.com/VIOLET-EVERGARDE/test" # device tree link
 export DT_BRANCH="android-9.0" # device tree branch
-export VENDOR="OPPO" # device manufacturer or vendor
+export VENDOR="vendor" # device manufacturer or vendor
 export TARGET="recoveryimage" # build method: recoveryimage, bootimage, vendorboot
 export TYPE="omni" # recovery type: omni, twrp, something else
 
